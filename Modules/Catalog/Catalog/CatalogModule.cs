347400dbdb6e0abe9943c931e43cd7c811929f2a
Thai Duong Huynh <huynhthaiduong21042003@gmail.com>
2024-11-21 22:29:45 +0700

    implement cqrs

--- Modules/Catalog/Catalog/CatalogModule.cs
+++ Modules/Catalog/Catalog/CatalogModule.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Data;
+using Shared.Data.Interceptors;
 
 namespace Catalog
 {
@@ -10,21 +10,29 @@ namespace Catalog
     {
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
             // Data - Infrastructure services
             var connectionString = configuration.GetConnectionString("Database");
 
-            
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
             services.AddDbContext<CatalogDbContext>((sp, options) =>
             {
                 options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
                 options.UseNpgsql(connectionString);
             });
 
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
+
             return services;
         }
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+            app.UseMigration<CatalogDbContext>();
             return app;
         }
+
     }
 }
---
+++ Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Products.Features.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string imageFile, decimal price) : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductHandler :IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
