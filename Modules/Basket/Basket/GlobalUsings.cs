a09a80feaa07344af54e0d16f17a2bc6e2551af6
Thai Duong Huynh <huynhthaiduong21042003@gmail.com>
2024-12-16 10:21:24 +0700

    config db

--- Modules/Basket/Basket/GlobalUsings.cs
+++ Modules/Basket/Basket/GlobalUsings.cs
@@ -1,2 +1,7 @@
 ﻿global using Shared.DDD;
+global using Basket.Basket.Models;
+global using Microsoft.EntityFrameworkCore;
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 
